5438d9ea737a457933d972414769de7d3b26030b
nfour64 <152326697+nfour64@users.noreply.github.com>
2023-12-04 18:34:36 -0500

    add quantity tracking to cart page

--- Cart.java
+++ Cart.java
@@ -4,18 +4,36 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Represents a shopping cart
+ */
 public class Cart {
+    /**
+     * A map to store the products in the cart and their quantities
+     */
     private Map<Product, Integer> items;
 
+    /**
+     * Constructor for the Cart class
+     */
     public Cart() {
         items = new HashMap<>();
     }
 
+    /**
+     * This method adds an item to the cart
+     * @param itemName The name of the item
+     * @param price The price of the item
+     */
     public void addItem(String itemName, Double price) {
         var item = new Product(itemName, price);
         items.put(item, items.getOrDefault(item, 0) + 1);
     }
 
+    /**
+     * This method removes an item from the cart
+     * @param itemName The name of the item
+     */
     public void removeItem(String itemName) {
         for (Product product : items.keySet()) {
             if (product.name.equals(itemName)) {
@@ -30,10 +48,18 @@ public class Cart {
         }
     }
 
+    /**
+     * This method returns a list of items in the cart
+     * @return The list of products
+     */
     public List<Product> getItems() {
         return new ArrayList<>(items.keySet());
     }
 
+    /**
+     * Gets the total price of items in the cart
+     * @return The total price
+     */
     public double getTotalPrice() {
         double total = 0;
         for (Map.Entry<Product, Integer> entry : items.entrySet()) {
@@ -42,6 +68,11 @@ public class Cart {
         return total;
     }
 
+    /**
+     * Returns the quantity of a specific product in the cart
+     * @param product The product
+     * @return The quantity of the product
+     */
     public int getItemQuantity(Product product) {
         return items.getOrDefault(product, 0);
     }
