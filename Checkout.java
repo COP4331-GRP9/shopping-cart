015fd5dcace3ce85789d2678a5b6ad690e3bb4d7
nfour64 <152326697+nfour64@users.noreply.github.com>
2023-12-04 19:04:15 -0500

    added javadoc comments

--- Checkout.java
+++ Checkout.java
@@ -2,17 +2,27 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * A Checkout window for the shopping cart application.
+ */
 public class Checkout {
     private JFrame frame;
     private JButton checkoutButton;
     private JTextArea orderSummaryTextArea;
 
+    /**
+     * Constructor for the Checkout class
+     * It initializes the GUI on the Event Dispatch Thread
+     */
     public Checkout() {
         SwingUtilities.invokeLater(() -> {
             createAndShowGUI();
         });
     }
 
+    /**
+     * Sets up the GUI components and makes the frame visible
+     */
     private void createAndShowGUI() {
         frame = new JFrame("Checkout");
         frame.setSize(400, 300);
@@ -25,6 +35,10 @@ public class Checkout {
         frame.setVisible(true);
     }
 
+    /**
+     * This method adds components to the panel
+     * @param panel The panel to which components are added
+     */
     private void placeComponents(JPanel panel) {
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
@@ -35,6 +49,10 @@ public class Checkout {
         panel.add(orderSummaryTextArea);
 
         checkoutButton.addActionListener(new ActionListener() {
+            /**
+             * Defines the action to be taken when the checkout button is clicked
+             * @param e The event that triggers this action
+             */
             @Override
             public void actionPerformed(ActionEvent e) {
                 // Add logic for processing the order and updating inventory
@@ -43,6 +61,10 @@ public class Checkout {
         });
     }
 
+    /**
+     * The main method that launches the Checkout window
+     * @param args Command-line arguments. Not used in this application
+     */
     public static void main(String[] args) {
         new Checkout();
     }
