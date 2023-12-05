4d346dd17b7513ca5ad94cf903fe0dcf0bf5660c
nfour64 <152326697+nfour64@users.noreply.github.com>
2023-12-05 08:38:36 -0500

    Added javadoc comments

--- AccountPage.java
+++ AccountPage.java
@@ -5,10 +5,18 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+/**
+ * This class represents the account page for a user on a shopping cart application
+ * The user can either be a customer or a seller
+ */
 public class AccountPage {
     private JFrame frame;
     private String userRole; // Customer or Seller
 
+    /**
+     * Constructs an AccountPage object and initializes the GUI
+     * @param userRole the role of the user, either "customer" or "seller"
+     */
     public AccountPage(String userRole) {
         this.userRole = userRole;
 
@@ -34,6 +42,10 @@ public class AccountPage {
         frame.setVisible(true);
     }
 
+    /**
+     * Displays the order history of a customer.
+     * @param panel the JPanel to which the order history is added
+     */
     private void displayCustomerOrderHistory(JPanel panel) {
         JTextArea orderHistoryArea = new JTextArea(15, 50);
         orderHistoryArea.setEditable(false);
@@ -50,6 +62,10 @@ public class AccountPage {
         }
     }
 
+    /**
+     * Displays the sales report of a seller
+     * @param panel the JPanel to which the sales report is added
+     */
     private void displaySellerSalesReport(JPanel panel) {
         JTextArea salesReportArea = new JTextArea(15, 50);
         salesReportArea.setEditable(false);
